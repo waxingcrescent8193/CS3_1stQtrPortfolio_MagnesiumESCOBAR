e19cd5508407ced5e87595a97e5e777cc6c4f50b
waxingcrescent8193 <b2029jezescobar@pshs.edu.ph>
2025-08-07 10:15:08 +0800

    1st non-graded exercise

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-//no tag needed !!
+let age;
+let maxAge;
+let numPerDay;
+let totalRequired;
+
+age = window.prompt("How old are you?");
+maxAge = window.prompt("Until what age will you live?");
+numPerDay = window.prompt("How much of your favorite snack do you consume daily?");
+
+totalRequired = (numPerDay*365)*(maxAge-age);
+
+document.write(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`);
