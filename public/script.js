9187b90225a69c074cb80d8ec6c1eee13416492d
waxingcrescent8193 <b2029jezescobar@pshs.edu.ph>
2025-08-07 10:22:03 +0800

    some edits + snack name

--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
-let age, maxAge, numPerDay, totalRequired;
+let age, maxAge, snackName, numPerDay, totalRequired;
 
 age = window.prompt("How old are you?");
 maxAge = window.prompt("Until what age will you live?");
+snackName = window.prompt("What is your favorite snack?");
 numPerDay = window.prompt("How much of your favorite snack do you consume daily?");
 
 totalRequired = (numPerDay*365)*(maxAge-age);
 
-document.write(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`);
+document.write(`You will need ${totalRequired} ${snackName} to last you until the ripe old age of ${maxAge}`);
