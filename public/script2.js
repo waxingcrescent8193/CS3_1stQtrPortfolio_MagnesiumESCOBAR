2bdfe943c3f9f2230f5d8ccf1a7fde6267d16493
waxingcrescent8193 <b2029jezescobar@pshs.edu.ph>
2025-08-14 10:14:20 +0800

    ng1 ver 3 (finally working !!)

--- public/script2.js
+++ public/script2.js
@@ -1,10 +1,8 @@
-let sure = window.confirm("Sure of your numbers?");
-if (sure) {
+function clickedButton() {
+    let age = document.getElementById("age").value;
+    let maxAge = document.getElementById("maxage").value;
+    let numPerDay = document.getElementById("numPerDay").value;
+
     let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge) - Number(age));
-    document.write ("You will need " + totalRequired +  " to last you until the ripe old age of " + maxAge); //display
-    alert(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`); //pop-up
-    console.log("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge); //console
-}
-else {
-    window.alert("Please click run again");
+    document.getElementById("resultthere").innerHTML = `You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`;
 }
